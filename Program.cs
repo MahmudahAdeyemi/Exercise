5273ea82f77f1523ce311fb9fe4b70769111cbd8
unknown <adeyemi.mahmudah@gmail.com>
2023-07-25 14:22:01 +0100

    yop

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int[ , ] array = new int[2,3]{{1,2,3},
+                             {2,4,5}
+                             };
+for (int row = 0; row < array.GetLength(0); row ++)
+{
+    for (int col = 0; col < array.GetLength(1); col ++ )
+    {
+        Console.Write(array[row,col]);
+    }
+    Console.WriteLine();
+}
